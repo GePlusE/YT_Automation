2f8f429e01bb52eeef1c7663db0f10ae8ca6feff
GePlusE <eisenbeiner@me.com>
2022-01-05 12:43:09 +0100

    finialized script + initial run

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import csv
+from re import sub
 
 from google.auth import credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -11,7 +12,6 @@ credentials = None
 
 # token.pickle stores the user's credentials from previously successful logins
 if os.path.exists("token.pickle"):
-    print("Loading Credentials From File...")
     with open("token.pickle", "rb") as token:
         credentials = pickle.load(token)
 
@@ -22,10 +22,8 @@ flow = InstalledAppFlow.from_client_secrets_file(
 # If there are no valid credentials available, then either refresh the token or log in.
 if not credentials or not credentials.valid:
     if credentials and credentials.expired and credentials.refresh_token:
-        print("Refreshing Access Token...")
         credentials.refresh(Request())
     else:
-        print("Fetching New Tokens...")
         flow = InstalledAppFlow.from_client_secrets_file(
             "client_secret.json", scopes=["https://www.googleapis.com/auth/youtube"]
         )
@@ -40,9 +38,7 @@ if not credentials or not credentials.valid:
             print("Saving Credentials for Future Use...")
             pickle.dump(credentials, f)
 
-
 youtube = build("youtube", "v3", credentials=credentials)
-
 
 # Collect subscripted channel ids
 sub_ids = []
@@ -50,11 +46,9 @@ request = youtube.subscriptions().list(part="snippet", maxResults=50, mine=True)
 response = request.execute()
 for item in response["items"]:
     sub_ids.append(item["snippet"]["resourceId"]["channelId"])
-print(sub_ids)
-sub_ids = ".".join(sub_ids)
 
-# only for testing purpose
-sub_ids = ["UCCezIgC97PvUuR4_gbFUs5g,UCEqCu7uFzlXaDKnfQeY5s-g"]
+# # only for testing purpose
+# sub_ids = ["UCCezIgC97PvUuR4_gbFUs5g,UCEqCu7uFzlXaDKnfQeY5s-g"]
 
 # load already liked video ids into set, to decrease amount of requests to YouTube
 with open("video_ids.csv", "r") as f:
@@ -62,32 +56,35 @@ with open("video_ids.csv", "r") as f:
     ids = list(reader)
 old_vid_ids = set([tuple(x) for x in ids])
 
-# loop through channels
+# loop through subscriped channels to get video ids
 for sub_id in sub_ids:
-    # request = youtube.channels().list(part="statistics", id=sub_id)
-    # response = request.execute()
     vid_ids = []
-    channelId = sub_id
-    maxResults = 1  # maxResults how many video will be liked per channel
-    request = youtube.search().list(
-        part="snippet",
-        channelId=channelId,
-        maxResults=maxResults,
-        order="date",
-        type="video",
-    )
+    request = youtube.channels().list(part="contentDetails", id=sub_id)
     response = request.execute()
-    for item in response["items"]:
-        print(item["snippet"]["title"])
-        vid_ids.append((item["id"]["videoId"], item["snippet"]["channelId"]))
+
+    # add playlist_id of latest uploads
+    uploads_id = []
+    for item in response["items"]:  # neccessary if user has more than one channel
+        uploads_id.append(item["contentDetails"]["relatedPlaylists"]["uploads"])
+
+    # loop through playlists to get video ids
+    for id in uploads_id:
+        request = youtube.playlistItems().list(
+            part="snippet, contentDetails", playlistId=id
+        )
+        response = request.execute()
+        for item in response["items"]:
+            channel_id = item["snippet"]["channelId"]
+            video_id = item["contentDetails"]["videoId"]
+            vid_ids.append((video_id, channel_id))
 
     # loop throuh videos and like + add new ones to set
     for vid_id in vid_ids:
         if vid_id not in old_vid_ids:
             youtube.videos().rate(rating="like", id=vid_id[0]).execute()
-            old_vid_ids.update(vid_id)
+            old_vid_ids.update([vid_id])
 
     # write new set to csv
-    with open("test.csv", "w") as f:
+    with open("video_ids.csv", "w") as f:
         writer = csv.writer(f, delimiter=";", quotechar="'")
         writer.writerows(old_vid_ids)
